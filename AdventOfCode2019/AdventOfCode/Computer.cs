cf6caa7ce54a5ffe25daba53dad1c96e40ca3d9e
S. Heidstra <heidstra@gmail.com>
2019-12-09 17:57:59 +0100

    dag 5

---
+++ AdventOfCode2019/AdventOfCode/Computer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode
+{
+
+    //Computer gemaakt op dag 2, daarna gebruikt op de dagen: 5
+    public class Computer
+    {
+        public Computer(int[] program, int input)
+        {
+            Input = input;
+            Result = compute(program, 0);
+
+        }
+
+        private int Input { get; set; }
+
+        public bool Debug { get; set; }
+
+        public int[] Result { get; private set; }
+
+        public int LastOutput { get; private set; }
+
+        public static (int, ParameterModes) interpretOp(int op)
+        {
+            string opString = op.ToString();
+            int opCode = Int32.Parse(opString.Substring(Math.Max(0, opString.Length - 2)));
+            ParameterModes parameterModes = new ParameterModes();
+
+            for (int i = 3; i < opString.Length + 1; i++)
+            {
+                parameterModes.Add(i - 2, Int32.Parse(opString.Substring(opString.Length - i, 1)));
+            }
+
+            return (opCode, parameterModes);
+        }
+
+        private int getValue(int[] program, int parameter, int mode)
+        {
+            //Immediate mode
+            if (mode == 1)
+            {
+                return program[parameter];
+            }
+
+            return program[program[parameter]];
+        }
+
+        private int[] compute(int[] program, int opPos)
+        {
+            var getCodes = interpretOp(program[opPos]);
+            int opCode = getCodes.Item1;
+            ParameterModes parameterModes = getCodes.Item2;
+            int offset = 4;
+
+
+            int[] result = program;
+
+            //Instructions
+            if (Debug) { Console.WriteLine("Debug opCode:" + opCode); }
+            switch (opCode)
+            {
+                case 1:
+                    // Console.WriteLine("Case 1");
+
+                    //Add numbers from 2 positions after the op code, write the result in position on 3
+                    int sum = getValue(program, opPos + 1, parameterModes.getMode(1)) + getValue(program, opPos + 2, parameterModes.getMode(2));
+                    result[program[opPos + 3]] = sum;
+                    break;
+                case 2:
+                    // Console.WriteLine("Case 2");
+                    int product = getValue(program, opPos + 1, parameterModes.getMode(1)) * getValue(program, opPos + 2, parameterModes.getMode(2));
+                    result[program[opPos + 3]] = product;
+                    break;
+                case 3:
+                    // Opcode 3 takes a single integer as input and saves it to the position given by its only parameter.For example,
+                    // the instruction 3,50 would take an input value and store it at address 50.
+                    result[program[opPos + 1]] = Input;
+                    offset = 2;
+                    break;
+
+                case 4:
+                    //Opcode 4 outputs the value of its only parameter.For example, the instruction 4,50 would output the value at address 50.
+                    LastOutput = getValue(program, opPos + 1, parameterModes.getMode(1));
+                    Console.WriteLine("Output: " + LastOutput);
+                    offset = 2;
+                    break;
+
+                case 5:
+                    //Opcode 5 is jump -if-true: if the first parameter is non - zero, it sets the instruction pointer to the value from the
+                    //second parameter.Otherwise, it does nothing.
+                    if (getValue(program, opPos + 1, parameterModes.getMode(1)) != 0)
+                    {
+                        opPos = getValue(program, opPos + 2, parameterModes.getMode(2));
+                        offset = 0;
+                    }
+                    else{offset = 3;}
+                    break;
+
+                case 6:
+                    //Opcode 6 is jump -if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second
+                    //parameter.Otherwise, it does nothing.
+                    if (getValue(program, opPos + 1, parameterModes.getMode(1)) == 0)
+                    {
+                        opPos = getValue(program, opPos + 2, parameterModes.getMode(2));
+                        offset = 0;
+                    }
+
+                    else{offset = 3;}
+                    break;
+
+                case 7:
+                    //Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the
+                    //third parameter.Otherwise, it stores 0.
+                    result[program[opPos + 3]] = getValue(program, opPos + 1, parameterModes.getMode(1)) <
+                                                 getValue(program, opPos + 2, parameterModes.getMode(2)) ?
+                            1 : 0;
+                    break;
+
+                case 8:
+                    //Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given
+                    //by the third parameter.Otherwise, it stores 0.
+                    result[program[opPos + 3]] = getValue(program, opPos + 1, parameterModes.getMode(1)) ==
+                                                 getValue(program, opPos + 2, parameterModes.getMode(2)) ?
+                        1 : 0;
+                    break;
+
+                case 99:
+                    //  Console.WriteLine("Case 99: stop execution!");
+                    return result;
+
+                default:
+                    Console.WriteLine(opCode);
+                    throw new Exception(opCode + " IS EEN FOUTE CODE GAST!");
+
+            }
+
+            result = compute(result, opPos + offset);
+
+            return result;
+        }
+
+    }
+
+    public class ParameterModes : Dictionary<int, int>
+    {
+        public int getMode(int key)
+        {
+            return base.ContainsKey(key) ? base[key] : 0;
+        }
+
+    }
+}
---
+++ AdventOfCode2019/AdventOfCode/ComputerTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace AdventOfCode
+{
+    [TestFixture]
+    class ComputerTests
+    {
+        [TestCase(new[] { 1, 0, 0, 0, 99 }, new[] { 2, 0, 0, 0, 99 })]
+        [TestCase(new[] { 2, 3, 0, 3, 99 }, new[] { 2, 3, 0, 6, 99 })]
+        [TestCase(new[] { 2, 4, 4, 5, 99, 0 }, new[] { 2, 4, 4, 5, 99, 9801 })]
+        [TestCase(new[] { 1, 1, 1, 4, 99, 5, 6, 0, 99 }, new[] { 30, 1, 1, 4, 2, 5, 6, 0, 99 })]
+        [TestCase(new[] { 1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50 }, new[] { 3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50 })]
+        [TestCase(new[]{ 1002, 4, 3, 4, 33 }, new[] { 1002, 4, 3, 4, 99 })]
+        public void testcomputer(int[] program, int[] result)
+        {
+            Assert.AreEqual(result, new Computer(program, 0).Result);
+        }
+
+
+        [TestCase(1002, 2, 0,1)]
+        [TestCase(5412, 12,4,5)]
+        public void testInterpretOp(int op, int resOp, int mode1, int mode2)
+        {
+            var parametermodes = Computer.interpretOp(op);
+            Assert.AreEqual(resOp, parametermodes.Item1);
+
+            Assert.AreEqual(mode1, parametermodes.Item2[1]);
+            Assert.AreEqual(mode2, parametermodes.Item2[2]);
+        }
+        
+        [TestCase(new[] { 3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8 }, 8, 1)]
+        [TestCase(new[] { 3, 3, 1108, -1, 8, 3, 4, 3, 99 }, 8, 1)]
+        [TestCase(new[] { 3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8 }, 7, 0)]
+        [TestCase(new[] { 3, 3, 1108, -1, 8, 3, 4, 3, 99 }, 7, 0)]
+        [TestCase(new[] { 3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8 }, 99, 0)]
+        [TestCase(new[] { 3, 3, 1108, -1, 8, 3, 4, 3, 99 }, 99, 0)]
+        [TestCase(new[] { 3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8 }, 8, 0)]
+        [TestCase(new[] { 3, 3, 1107, -1, 8, 3, 4, 3, 99 }, 8, 0)]
+        [TestCase(new[] { 3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8 }, 7, 1)]
+        [TestCase(new[] { 3, 3, 1107, -1, 8, 3, 4, 3, 99 }, 7, 1)]
+        [TestCase(new[] { 3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8 }, 99, 0)]
+        [TestCase(new[] { 3, 3, 1107, -1, 8, 3, 4, 3, 99 }, 99, 0)]
+        [TestCase(new[] { 3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9 }, 8, 1)]
+        [TestCase(new[] { 3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1 }, 8, 1)]
+        [TestCase(new[] { 3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9 }, 0,0)]
+        [TestCase(new[] { 3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1 }, 0, 0)]
+        [TestCase(new[] { 3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9 },99, 1)]
+        [TestCase(new[] { 3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1 }, 99, 1)]
+
+        [TestCase(new[] { 3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99 }, 7, 999)]
+        [TestCase(new[] { 3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99 }, 8, 1000)]
+        [TestCase(new[] { 3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99 }, 9, 1001)]
+        public void jumpAndEqualTests(int[] testprogram, int input, int result)
+        {
+            Computer comp8 = new Computer(testprogram, input);
+            Assert.AreEqual(result, comp8.LastOutput);
+        }
+    }
+}
